ed54ba5e281a97f2633285f8cc615e5c23950ba3
YSXCC <wshysxcc@outlook.com>
2023-06-30 18:51:12 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- 基础配置
+require("basic")
+
+-- 按键
+require("keymaps")
+
+-- 插件
+require("plugins.plugins-setup")
+require("plugins.lualine")
+require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.lsp")
+require("plugins.cmp")
